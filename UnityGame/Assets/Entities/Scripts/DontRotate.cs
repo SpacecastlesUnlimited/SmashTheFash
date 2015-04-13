f4af4b570703c6fc1b830db89ec28e43a4be8885
Stefan Insam <ramsesoriginal@gmail.com>
2015-04-14 01:49:04 +0200

    Added Sprites, Fashists, Path, Multiplatform Builds

---
+++ UnityGame/Assets/Entities/Scripts/DontRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = new Vector3(90,0,0);
+	}
+}
---
+++ UnityGame/Assets/Entities/Scripts/MoveAlongPath.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAlongPath : MonoBehaviour {
+
+	public Transform[] waypoints;
+	public float tolerance;
+	public bool reachedEnd;
+	private NavMeshAgent agent;
+	private Transform waypoint;
+	private int pos;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		foreach (var p in waypoints)
+			p.gameObject.SetActive (false);
+		pos = 0;
+		waypoint = waypoints [pos];
+		waypoint.gameObject.SetActive(true);
+		agent.SetDestination (waypoint.position);
+		reachedEnd = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!reachedEnd && (transform.position - waypoint.position).magnitude <= tolerance) {
+			waypoint.gameObject.SetActive(false);
+			pos++;
+			if (pos >= waypoints.Length) {
+				reachedEnd = true;
+				return;
+			}
+			waypoint = waypoints [pos];
+			waypoint.gameObject.SetActive(true);
+			agent.SetDestination (waypoint.position);
+		}
+	}
+}
