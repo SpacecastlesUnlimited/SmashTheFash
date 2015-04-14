a72bb76229697b38963b6ba83d5cdfe4f8c2006f
Stefan Insam <ramsesoriginal@gmail.com>
2015-04-14 15:25:25 +0200

    Improving Screen to World translation

--- UnityGame/Assets/Entities/Scripts/Place.cs
+++ UnityGame/Assets/Entities/Scripts/Place.cs
@@ -7,18 +7,22 @@ public class Place : MonoBehaviour {
 	void Start () {
 	
 	}
+
+	void GetPoint(float screenX, float ScreenY) {
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(screenX,ScreenY,0));
+		RaycastHit hitInfo;
+		var layerMask = 1 << LayerMask.NameToLayer ("Clickable");
+		Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+		if (Physics.Raycast (ray, out hitInfo,100,layerMask)) {
+			Debug.Log (hitInfo.point);
+			//target.position = hitInfo.point; //new Vector3(hitInfo.point.x, 0.0f, hitInfo.point.z );
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
-			Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0));
-			RaycastHit hitInfo;
-			var layerMask = 1 << LayerMask.NameToLayer ("Ground");
-			Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
-			if (Physics.Raycast (ray, out hitInfo,float.PositiveInfinity,layerMask)) {
-				Debug.Log (hitInfo.point);
-				//target.position = hitInfo.point; //new Vector3(hitInfo.point.x, 0.0f, hitInfo.point.z );
-			}
+			GetPoint(Input.mousePosition.x, Input.mousePosition.y);
 		}
 	}
 }
