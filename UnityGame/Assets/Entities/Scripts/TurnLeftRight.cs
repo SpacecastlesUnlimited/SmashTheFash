e848198fbf5dd21edf97eb4b4f8990423b2db668
Stefan Insam <ramsesoriginal@gmail.com>
2015-04-14 21:51:50 +0200

    Started Working on Having images for left/right

---
+++ UnityGame/Assets/Entities/Scripts/TurnLeftRight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnLeftRight : MonoBehaviour {
+
+	public Sprite left;
+	public Sprite right;
+	public SpriteRenderer image;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.forward.x < 0)
+			image.sprite = left;
+		else
+			image.sprite = right;
+	}
+}
